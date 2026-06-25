bd571a8621b4dad4f64e6e0d3805fe69ad076322
pavelbarhotov <pavelbarhotov@yandex.ru>
2026-06-25 23:30:10 +0300

    Added linters and modified routes

--- src/apps/booking/views.py
+++ src/apps/booking/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse('<h1>Index page</h1>')
+
+def create(request):
+    return HttpResponse({'status': 'ok', 'session': 1})
